c573b376067527e66b40583464de0e84824dbe6f
Lyndon-Li <lyonghui@vmware.com>
2024-11-19 13:27:25 +0800

    local build

--- hostprocess/csi-proxy/build.sh
+++ hostprocess/csi-proxy/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-args=$(getopt -o v:p -l version:,push -- "$@")
+args=$(getopt -o v:r:s:b:p -l version:,repository:,source:,branch:,push -- "$@")
 eval set -- "$args"
 
 while [ $# -ge 1 ]; do
@@ -13,6 +13,18 @@ while [ $# -ge 1 ]; do
     -v|--version)
       version="$2"
       shift 
+      ;;
+    -r|--repository)
+      repository="$2"
+      shift
+      ;;
+    -s|--source)
+      source="$2"
+      shift
+      ;;
+    -b|--branch)
+      branch="$2"
+      shift
       ;;
     -p|--push)
       push="1"
@@ -35,12 +47,19 @@ if [[ "$push" == "1" ]]; then
 fi
 
 repository=${repository:-"ghcr.io/kubernetes-sigs/sig-windows"}
+echo "Using repository ${repository}"
+
+echo "Using source ${source}"
+
+echo "Using branch ${branch}"
+
+echo "Using output ${output}"
 
 set -x
 
-docker buildx create --name img-builder --use --platform windows/amd64
-trap 'docker buildx rm img-builder' EXIT
+docker buildx create --name img-builder-csi-proxy --use --platform windows/amd64
+trap 'docker buildx rm img-builder-csi-proxy' EXIT
 
 
-docker buildx build --platform windows/amd64 --output=$output -f Dockerfile.windows -t ${repository}/csi-proxy:${version} .
+docker buildx build --platform windows/amd64 --output=$output --build-arg=CSI_PROXY_SOURCE=${source} --build-arg=CSI_PROXY_BRANCH=${branch} -f Dockerfile.windows -t ${repository}/csi-proxy:${version} .
 
